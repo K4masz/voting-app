ba9b2bab18a2c7c8f8c13ef9fac9da6fa93e0a3f
Tomasz Pawlik <tomasz.pawlik96@gmail.com>
2023-09-13 18:05:04 +0200

    add basic voting component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { VotingComponent } from './containers/voting/voting.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VotingComponent
   ],
   imports: [
     BrowserModule,
