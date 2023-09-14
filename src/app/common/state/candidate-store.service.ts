b281b8b1bd4699113ddd05a3b90b13d6ab7b093f
Tomasz Pawlik <tomasz.pawlik96@gmail.com>
2023-09-14 20:58:22 +0200

    move the inital data stores states to constants

--- src/app/common/state/candidate-store.service.ts
+++ src/app/common/state/candidate-store.service.ts
@@ -1,28 +1,27 @@
 import { Injectable } from '@angular/core';
-import { Candidate } from 'src/app/model/candidate';
 import { BehaviorSubject, Observable } from 'rxjs';
+import { Candidate } from 'src/app/model/candidate';
+
+export const CANDIDATE_STORE_INITIAL_STATE: Candidate[] = [
+  { name: 'Johnny Bravo', numOfVotes: 2 },
+  { name: 'Pluto', numOfVotes: 5 }
+];
 
 @Injectable({
   providedIn: 'root'
 })
 export class CandidateStoreService {
 
-  private _candidates: BehaviorSubject<Candidate[]> = new BehaviorSubject<Candidate[]>([]);
+  private _candidates: BehaviorSubject<Candidate[]> = new BehaviorSubject<Candidate[]>(CANDIDATE_STORE_INITIAL_STATE);
   public readonly candidates$: Observable<Candidate[]> = this._candidates.asObservable();
 
-  constructor() {
-    this._candidates.next([
-      { name: 'Johnny Bravo', numOfVotes: 2 },
-      { name: 'Pluto', numOfVotes: 5 }
-    ]);
-  }
+  constructor() { }
 
   addCandidate(candidate: Candidate): void {
     this._candidates.next([...this._candidates.getValue(), candidate])
   }
 
   increaseVotesNumber(candidateName: string): void {
-
     const currentCandidates: Candidate[] = this._candidates.getValue();
 
     const candidate = currentCandidates.find(candidate => candidate.name === candidateName)!;
--- src/app/common/state/voter-store.service.ts
+++ src/app/common/state/voter-store.service.ts
@@ -2,28 +2,28 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { Voter } from 'src/app/model/voter';
 
+export const VOTER_STORE_INITAL_STATE: Voter[] = [
+  { name: 'Peppa', voted: false },
+  { name: 'Rumcajs', voted: false },
+  { name: 'Rebel', voted: true }
+]
+
 @Injectable({
   providedIn: 'root'
 })
 export class VoterStoreService {
 
-  private _voters: BehaviorSubject<Voter[]> = new BehaviorSubject<Voter[]>([]);
+  private _voters: BehaviorSubject<Voter[]> = new BehaviorSubject<Voter[]>(VOTER_STORE_INITAL_STATE);
   public readonly voters$: Observable<Voter[]> = this._voters.asObservable();
 
-  constructor() {
-    this._voters.next([
-      { name: 'Peppa', voted: false },
-      { name: 'Rumcajs', voted: false },
-      { name: 'Rebel', voted: true}
-    ])
-  }
+  constructor() { }
 
-  addVoter(data: Voter){
+  addVoter(data: Voter): void {
     this._voters.next([...this._voters.getValue(), data])
   }
 
-  markAsHasVoted(voterName: string):void{
-    const currentVoters:Voter[] = this._voters.getValue();
+  markAsHasVoted(voterName: string): void {
+    const currentVoters: Voter[] = this._voters.getValue();
 
     const voter = currentVoters.find(voter => voter.name === voterName)!;
     const newVoters = currentVoters.filter(voter => voter.name !== voterName);
