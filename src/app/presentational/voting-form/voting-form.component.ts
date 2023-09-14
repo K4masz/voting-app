4b1d3187b24a8f6a5659a5ed6bb191b0cd49698e
Tomasz Pawlik <tomasz.pawlik96@gmail.com>
2023-09-14 20:56:34 +0200

    add getters for form controls

--- src/app/presentational/voting-form/voting-form.component.ts
+++ src/app/presentational/voting-form/voting-form.component.ts
@@ -1,6 +1,7 @@
-import { ChangeDetectionStrategy, Component, Input, Output, EventEmitter } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Candidate } from 'src/app/model/candidate';
+import { Vote } from 'src/app/model/vote';
 import { Voter } from 'src/app/model/voter';
 
 @Component({
@@ -14,20 +15,25 @@ export class VotingFormComponent {
   @Input() voters: Voter[] = [];
   @Input() candidates: Candidate[] = [];
 
-  @Output() voteEvent: EventEmitter<{ voter: string, candidate: string }> = new EventEmitter();
+  @Output() voteEvent: EventEmitter<Vote> = new EventEmitter<Vote>();
 
   votingForm: FormGroup = this.formBuilder.group({
     voter: ['', [Validators.required]],
     candidate: ['', [Validators.required]]
   })
 
-  constructor(private formBuilder: FormBuilder) {
-
-  }
+  constructor(private formBuilder: FormBuilder) { }
 
   submit(): void {
     this.voteEvent.emit(this.votingForm.value);
     this.votingForm.reset();
-    this.votingForm.updateValueAndValidity();
+  }
+
+  get voterControl() {
+    return this.votingForm.controls['voter'];
+  }
+
+  get candidateControl() {
+    return this.votingForm.controls['candidate'];
   }
 }
